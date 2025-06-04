50a2b01c5d391e4ab4abb09520b89f9019b58f2b
Frank Paucar <frankmichael.paucarcaceres.contractor@gdevtools.com>
2025-06-03 22:34:41 -0500

    primer commit

---
+++ src/main/java/tech/leori/consulting/java/arq/clean/gestion/planillas/GestionPlanillasV1Application.java
@@ -0,0 +1,13 @@
+package tech.leori.consulting.java.arq.clean.gestion.planillas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPlanillasV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPlanillasV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/leori/consulting/java/arq/clean/gestion/planillas/GestionPlanillasV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.leori.consulting.java.arq.clean.gestion.planillas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPlanillasV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
